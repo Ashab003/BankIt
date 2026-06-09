fd2d97f7a9aa41b7acb34da8cd5c4d904ce63c7a
ashab <ashab.00042@gmail.com>
2026-06-09 17:33:18 +0300

    Phase 6, Trasactional Controller

--- src/main/java/com/project/BankIt_backend/controller/TransactionController.java
+++ src/main/java/com/project/BankIt_backend/controller/TransactionController.java
@@ -1,10 +1,28 @@
 package com.project.BankIt_backend.controller;
 
+import com.project.BankIt_backend.dto.TransferRequestDTO;
+import com.project.BankIt_backend.service.PaymentService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/transaction")
+@RequestMapping("/api/transactions")
+@RequiredArgsConstructor
 public class TransactionController {
 
+    private final PaymentService paymentService;
+
+    @PostMapping("/transfer")
+    public ResponseEntity<String> transferMoney(
+            @Valid @RequestBody TransferRequestDTO transferRequestDTO) {
+
+        paymentService.transferMoney(transferRequestDTO);
+
+        return ResponseEntity.ok("Transfer completed successfully");
+    }
 }
